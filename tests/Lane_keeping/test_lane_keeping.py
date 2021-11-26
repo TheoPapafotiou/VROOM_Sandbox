82d18b11efe23cca9a8d41abc3b1307d114f71eb
Papalight <theo.papafotiou@gmail.com>
2021-11-26 23:56:05 +0200

    Last one for today

--- tests/Lane_keeping/test_lane_keeping.py
+++ tests/Lane_keeping/test_lane_keeping.py
@@ -4,8 +4,11 @@
 import unittest
 import cv2
 
+from ...src.lane_keeping import LaneKeeping
+
 class TestLaneKeeping(unittest.TestCase):
     def setUp(self):
+        self.laneKeeping = LaneKeeping()
         self.desired_angles = [0.22, 0.03, 0.06, -0.01]
         self.path = "Lane_keeping_inputs/image_" 
 
@@ -13,7 +16,7 @@ class TestLaneKeeping(unittest.TestCase):
         try:
             for i in range(0, 4):
                 image = cv2.imread(self.path+str(i))
-                angle = self.desired_angles[i] * 100
+                angle = self.laneKeeping(image)
                 message = "Lane keeping failed in image_" + str(i)
                 self.assertAlmostEqual(angle/100, self.desired_angles[i], 1, msg=message)
         except Exception as e:
