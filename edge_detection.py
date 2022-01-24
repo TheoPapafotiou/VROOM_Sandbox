3de059fbb5a9b89dd7e5b8ed2ac92765dd2f7f64
giotaChita <xhgiota@gmail.com>
2022-01-24 17:08:24 +0200

    ransac algo

--- edge_detection.py
+++ edge_detection.py
@@ -1,428 +1,125 @@
 # edge detection 
+from cgitb import grey
+from re import X
+from xml.etree.ElementPath import xpath_tokenizer
 import cv2
 import matplotlib.pyplot as plt 
+from matplotlib import pyplot as plt
 import numpy as np
 from numpy.core.overrides import verify_matching_signatures
+from masksForIntersection import Mask_intesecrtion
+import skimage
+from skimage.measure import ransac, LineModelND
+from sklearn import linear_model, datasets
 
 
-def canny(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # reduce noise  -> filter with guassian filter
-    blur = cv2.GaussianBlur(gray,(5,5),0)
-    #canny method to find lanes -> based on colour
-    canny = cv2.Canny(blur, 50, 150)
-    return canny
+import time 
 
-'''
-First Way (+)
-image = plt.imread('kernel_test.png')
-plt.imshow(image)
-
-vertical_filter = [[-1, 2, -1],[0, 0, 0],[1, 2, 1]]
-
-horizontal_filter =[[-1, 0, -1],[-2, 0, 2],[-1, 0, 1]]
-
-n,m,d = image.shape
-
-
-edges_img = np.zeros_like(image)
-
-for row in range(3,n-2):
-    for col in range(3,m-2): #i chose to ignore the edges of the img because there is not enough pixels to apply my 3x3 arrayfilter
-        local_pixels = image[row-1:row+2, col-1:col+2, 0] 
-
-        v_trasformed_pixels = vertical_filter*local_pixels
-        vertical_score = v_trasformed_pixels.sum() + 4 #I can take values between -4,4 so normilization 
-        # zero to one range
-
-        h_trasformed_pixels = horizontal_filter * local_pixels
-        horizontal_score = h_trasformed_pixels.sum()/4
-
-        edge_score = (vertical_score**2 + horizontal_score**2)**.5
-        edges_img[row,col] = [edge_score]*3
-
-edges_img = edges_img/edges_img.max()
-plt.imshow(edges_img)
-'''
-'''
-SECOND WAY (++)
-img = cv22.imread("kernel_test.png")
-cv22.imshow('img', img)
-gray = cv22.cv2tColor(img, cv22.COLOR_BGR2GRAY)
-gray = np.float32(gray)
-dst = cv22.cornerHarris(gray, 2,3,0.04)
-
-dst = cv22.dilate(dst,None)
-
-img[dst>0.01*dst.max()] = [0, 0, 255]
-cv22.imshow('dst', img)
-
-if cv22.waitKey(0) & 0xff == 27:
-    cv22.destroyAllWindows()
-
-    '''
-'''
-# Third way - (+++)
-
-from matplotlib import pyplot as plt
-def canny(image):
-    gray = cv22.cv2tColor(image, cv22.COLOR_RGB2GRAY)
-    # reduce noise  -> filter with guassian filter
-    blur = cv22.GaussianBlur(gray,(5,5),0)
-    #canny method to find lanes -> based on colour
-    canny = cv22.Canny(blur, 50, 150)
-    return canny
-  
-def region_of_interest(image,option):
-    #mask
-    height = image.shape[0]
-    width = image.shape[1]
-    #for options == 1 when the car moves straight
-    if option == 1:
-        polygons=np.array([
-        [(0,height),(int(width/2.5),int(height/3.5)),(int(width/1.6),int(height/3.5)),(width,height)] #(y,x)
-        ])
-    elif option == 2: #options == 2 when the car turn right small
-        polygons=np.array([
-        [(int(width/1.65), int(height/1.68)),(width,int(height/4.1)),(width,height),(int(width/1.65),height)] #(y,x)
-        ])
-    elif option == 3:
-        polygons=np.array([
-        [(1,1),(650,350),(1030,350),(width,height)] #(y,x)
-        ])
-
-    # apply it to a block mask
-    mask = np.zeros_like(image)
-    cv22.fillPoly(mask, polygons, 255)
-    masked_image = cv22.bitwise_and(image,mask)
-    return masked_image
-    
 x_points = []
 y_points = []
 
-cap = cv22.VideoCapture("straight_simulation.mp4")
-while (cap.isOpened()):
-    _,frame = cap.read()
-    canny_image = canny(frame)
-    cropped_image = region_of_interest(canny_image,1)
-    #cv22.imshow("result", cropped_image)
-    width  = cap.get(cv22.CAP_PROP_FRAME_WIDTH)   # float `width`
-    height = cap.get(cv22.CAP_PROP_FRAME_HEIGHT)  # float `height`
-    corners = cv22.goodFeaturesToTrack(cropped_image, 27, 0.01, 10)
-    corners = np.int0(corners)
+def ransac_method(x_points, y_points):
+    data = np.column_stack([x_points,y_points])
 
-  
-    # we iterate through each corner, 
-    # making a circle at each point that we think is a corner.
+    model = LineModelND()
+    model.estimate(data)
+    # robustly fit line only using inlier data with RANSAC algorithm
+    model_robust, inliers = ransac(data, LineModelND, min_samples=2,
+                               residual_threshold=1, max_trials=1000)
+    outliers = inliers == False
+    line_x = x_points
+    line_y = model.predict_y(line_x)
+    line_y_robust = model_robust.predict_y(line_x)
+    return line_x, line_y_robust
 
-    for i in corners:
-        x, y = i.ravel()
-        x_points.append(x)
-        y_points.append(y)
-        cv22.circle(cropped_image, (x, y), 3, 255, -1)
-    white = (255,255,255)
-    
-    cv22.line(cropped_image, (int(x_points[2]),int(y_points[2])), (50,int(height)), white, 15)
-    cv22.imshow("result", cropped_image)
-
-    if cv22.waitKey(5) == ord('q'):
-        break
-cap.release()
-cv22.destroyAllWindows()
 '''
-
-# tetartos tropos -> somehow operating 
-
-from matplotlib import pyplot as plt
-def canny(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    # reduce noise  -> filter with guassian filter
-    blur = cv2.GaussianBlur(gray,(5,5),0)
-    #canny method to find lanes -> based on colour
-    canny = cv2.Canny(blur, 50, 150)
-    return canny
-  
-def region_of_interest(image,option):
-    #mask
-    height = image.shape[0]
-    width = image.shape[1]
-    #for options == 1 when the car moves straight
-    if option == 1:
-        polygons=np.array([
-        [(0,height),(int(width/2.5),int(height/3.5)),(int(width/1.6),int(height/3.5)),(width,height)] #(y,x)
-        ])
-    elif option == 2: #options == 2 when the car turn right small
-        polygons=np.array([
-        [(int(width/1.65), int(height/1.68)),(width,int(height/4.1)),(width,height),(int(width/1.65),height)] #(y,x)
-        ])
-    elif option == 3:
-        polygons=np.array([
-        [(1,1),(650,350),(1030,350),(width,height)] #(y,x)
-        ])
-
-    # apply it to a block mask
-    mask = np.zeros_like(image)
-    cv2.fillPoly(mask, polygons, 255)
-    masked_image = cv2.bitwise_and(image,mask)
-    return masked_image
-def slope(x1,y1,x2,y2):
-    if x2!=x1:
-        return((y2-y1)/(x2-x1))
-    else:
-        return 'NA'
-def drawLine(img, x1,y1,x2,y2):
-    m = slope(x1,y1,x2,y2)
-    h,w = img.shape[: 2]
-    if m != 'NA':
-
-        # starting point
-        px = 0
-        py = -(x1-0)*m + y1
-        # ending point
-        qx = w
-        qy = -(x2-w)*m + y2
-        cv2.line(img,(int(px), int(py)), (int(qx),int(qy)), (255,255,255), 12)
-
+    fig, ax = plt.subplots()
+    ax.plot(data[inliers, 0], data[inliers, 1], '.b', alpha=0.6,
+            label='Inlier data')
+    ax.plot(data[outliers, 0], data[outliers, 1], '.r', alpha=0.6,
+            label='Outlier data')
+    ax.plot(line_x, line_y, '-k', label='Line model from all data')
+    ax.plot(line_x, line_y_robust, '-b', label='Robust line model')
+    ax.legend(loc='lower left')
+    #plt.show()
+ '''
     
-x_points = []
-y_points = []
 
-x_points2 = []
-y_points2 = []
 
-cap = cv2.VideoCapture("straight_simulation.mp4")
 
-while (cap.isOpened()):
-    _,frame = cap.read()
-    #canny_image = canny(frame)
-    
-    #cropped_image = region_of_interest(canny_image,1)
-    width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float `width`
-    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float `height`
-    
-    frame1 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame2 = np.float32(frame1)
-    frame2 = region_of_interest(frame2,1)
-    dst = cv2.cornerHarris(frame2, 2,3,0.04)
+
+def corner_3(gray,turn): #turn = 1 : straight, turn = 2 : small right, turn = 3 : left big
+    dst = cv2.cornerHarris(gray, 2,3,0.04)
     dst = cv2.dilate(dst,None)
     ret, dst = cv2.threshold(dst,0.01*dst.max(),255,0)
     dst = np.uint8(dst)
+    
 # find centroids
     ret,lables, stats, centroids = cv2.connectedComponentsWithStats(dst)
     criteria = (cv2.TERM_CRITERIA_EPS +cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-    corners = cv2.cornerSubPix(frame2,np.float32(centroids),(5,5),(-1,-1),criteria)
-    res = np.hstack((corners,centroids))
+    corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
+    res = np.hstack((centroids,corners))
     res = np.int0(res)
-   # frame[res[:,1],res[:,0]] = [0,0,255]
+   
 
-    frame[dst>0.01 * dst.max()] = [0,0,255]
     for i in res:
         x,y,x2,y2 = i.ravel() 
-        
-        if x == x2 and y == y2:
+        if turn == 1:
             x_points.append(x)
-            y_points.append(y)
-            cv2.circle(frame2, (x, y), 3, 255, -1)
+            y_points.append(y)            
+            cv2.circle(gray, (x, y), 3, 255, -1)
+        elif turn == 2:
+            if x == x2 and y == y2:
+#                data = np.column_stack([x,y])
+                x_points.append(x)
+                y_points.append(y) 
+                data = np.column_stack([x_points,y_points])           
+                cv2.circle(gray, (x, y), 3, 255, -1)
 
+                
     white = (255,255,255)
+    xnew,ynew = ransac_method(x_points,y_points)
+    if len(x_points) > 2:
+        cv2.line(gray, (0, int(height)),(int(xnew[0]), int(ynew[0])), white, 9) 
+    '''
+    if len(x_points) > 2:
+        cv2.line(gray, (0, int(height)),(x_points[1], y_points[1]), white, 9)
+    '''
+    return gray
+
+
     
-    cv2.line(frame, (int(width/8), int(height)),(x_points[0], y_points[0]), white, 9)
-    corners2 = cv2.goodFeaturesToTrack(frame2, 27, 0.01, 10)
-    corners2 = np.int0(corners)
-  
-    # we iterate through each corner, 
-    # making a circle at each point that we think is a corner.
-
-    for i in corners:
-        x, y = i.ravel()
-        x_points2.append(x)
-        y_points2.append(y)
-        cv2.circle(frame2, (int(x), int(y)), 3, 255, -1)
-
-#    print(y_points[2])
-    white = (255,255,255)
+cap = cv2.VideoCapture("videos/straight.mp4")
+time_start = time.time()
+while (cap.isOpened()):
+    _,frame = cap.read()
+    gray = Mask_intesecrtion.canny(frame)
+    width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float `width`
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float `height`
+    cropped_frame = Mask_intesecrtion.region_of_interest(gray,1)
     
-    #drawLine(cropped_image,50,int(height),544,233)
-    cv2.line(frame, (int(width/8),int(height)), (int(x_points2[0]),int(y_points2[0])), (0,0,255), 10  )
+    gray= corner_3(cropped_frame,1)
+   # ransac_method(x_points,y_points)
+    
+    cv2.imshow("result", gray)
 
-    cv2.imshow("result", frame)
-
-    if cv2.waitKey(5) == ord('q'):
+    if cv2.waitKey(10) == ord('q'):
         break
-
+    
 cv2.destroyAllWindows()
 
 
-
-'''
-img = cv2.imread('straight_2.png')
-cv2.namedWindow('Harris Corner Detection Test', cv2.WINDOW_NORMAL)
-
-def f(x=None):
-    return
-
-cv2.createTrackbar('Harris Window Size', 'Harris Corner Detection Test', 5, 25, f)
-cv2.createTrackbar('Harris Parameter', 'Harris Corner Detection Test', 1, 100, f)
-cv2.createTrackbar('Sobel Aperture', 'Harris Corner Detection Test', 1, 14, f)
-cv2.createTrackbar('Detection Threshold', 'Harris Corner Detection Test', 1, 100, f)
-
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-gray = np.float32(gray)
-
-img_bak = img
-
-while True:
-    img = img_bak.copy()
-
-    window_size = cv2.getTrackbarPos('Harris Window Size', 'Harris Corner Detection Test')
-    harris_parameter = cv2.getTrackbarPos('Harris Parameter', 'Harris Corner Detection Test')
-    sobel_aperture = cv2.getTrackbarPos('Sobel Aperture', 'Harris Corner Detection Test')
-    threshold = cv2.getTrackbarPos('Detection Threshold', 'Harris Corner Detection Test')
-
-    sobel_aperture = sobel_aperture * 2 + 1
-
-    if window_size <= 0:
-        window_size = 1
-
-    dst = cv2.cornerHarris(gray, window_size, sobel_aperture, harris_parameter/100)
-
-    # Threshold for an optimal value, it may vary depending on the image.
-    _ , dst_thresh = cv2.threshold(dst, threshold/100 * dst.max(), 255, 0)
-    dst_thresh = np.uint8(dst_thresh)
-
-    dst_show = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
-    dst_show = np.uint8(dst_show)
-
-    ## REFINE CORNERS HERE!
-
-    # find centroids
-    ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst_thresh)
-
-    # define the criteria to stop and refine the corners
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-    corners = cv2.cornerSubPix(gray, np.float32(centroids), (5,5), (-1,-1), criteria)
-
-    try:
-        # Now draw them
-        corners = np.int0(corners)
-
-        img[corners[:, 1], corners[:, 0]] = [0, 255, 0]
-        img[dst_thresh > 1] = [0, 0, 255]
-
-    except:
-        pass
-
-    cv2.imshow('Harris Corner Detection Test', np.hstack((img, dst_show)))
-
-    if cv2.waitKey(10) & 0xFF == 27:
-        break
-
-cv2.destroyAllWindows()
-'''
-'''
-import cv2
-import numpy as np
-
-img_file = 'straight_1.png'
-img = cv2.imread(img_file, cv2.IMREAD_COLOR)
-
-imgDim = img.shape
-dimA = imgDim[0]
-dimB = imgDim[1]
-
-# RGB to Gray scale conversion
-img_gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-# Noise removal with iterative bilateral filter(removes noise while preserving edges)
-noise_removal = cv2.bilateralFilter(img_gray,9,75,75)
-# Thresholding the image
-ret,thresh_image = cv2.threshold(noise_removal,220,255,cv2.THRESH_OTSU)
-th = cv2.adaptiveThreshold(noise_removal, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-
-# Applying Canny Edge detection
-canny_image = cv2.Canny(th,250,255)
-canny_image = cv2.convertScaleAbs(canny_image)
-
-# dilation to strengthen the edges
-kernel = np.ones((3,3), np.uint8)
-# Creating the kernel for dilation
-dilated_image = cv2.dilate(canny_image,kernel,iterations=1)
-np.set_printoptions(threshold=np.nan)
-
-_, contours, h = cv2.findContours(dilated_image, 1, 2)
-contours= sorted(contours, key = cv2.contourArea, reverse = True)[:1]
-
-
-corners    = cv2.goodFeaturesToTrack(thresh_image,6,0.06,25)
-corners    = np.float32(corners)
-
-for item in corners:
-    x,y    = item[0]
-    cv2.circle(img,(x,y),10,255,-1)
-cv2.namedWindow("Corners", cv2.WINDOW_NORMAL)
-cv2.imshow("Corners",img)
-cv2.waitKey()
-'''
-'''
-import numpy as np
-import cv2 as cv
-
-filename = 'straight_1.png'
-img = cv.imread(filename)
-gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-gray = np.float32(gray)
-dst = cv.cornerHarris(gray,2,3,0.04)
-#result is dilated for marking the corners, not important
-dst = cv.dilate(dst,None)
-# Threshold for an optimal value, it may vary depending on the image.
-img[dst>0.01*dst.max()]=[0,0,255]
-cv.imshow('dst',img)
-if cv.waitKey(0) & 0xff == 27:
-    cv.destroyAllWindows()
-'''
-'''
-from matplotlib import pyplot as plt
-
-def canny(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    # reduce noise  -> filter with guassian filter
-    blur = cv2.GaussianBlur(gray,(5,5),0)
-    #canny method to find lanes -> based on colour
-    canny = cv2.Canny(blur, 50, 150)
-    return canny
-  
-def region_of_interest(image,option):
-    #mask
-    height = image.shape[0]
-    width = image.shape[1]
-    #for options == 1 when the car moves straight
-    if option == 1:
-        polygons=np.array([
-        [(0,height),(int(width/2.5),int(height/3.5)),(int(width/1.6),int(height/3.5)),(width,height)] #(y,x)
-        ])
-    elif option == 2: #options == 2 when the car turn right small
-        polygons=np.array([
-        [(int(width/1.65), int(height/1.68)),(width,int(height/4.1)),(width,height),(int(width/1.65),height)] #(y,x)
-        ])
-    elif option == 3:
-        polygons=np.array([
-        [(1,1),(650,350),(1030,350),(width,height)] #(y,x)
-        ])
-
-    # apply it to a block mask
-    mask = np.zeros_like(image)
-    cv2.fillPoly(mask, polygons, 255)
-    masked_image = cv2.bitwise_and(image,mask)
-    return masked_image
+"""
 def slope(x1,y1,x2,y2):
     if x2!=x1:
         return((y2-y1)/(x2-x1))
     else:
         return 'NA'
+
 def drawLine(img, x1,y1,x2,y2):
     m = slope(x1,y1,x2,y2)
     h,w = img.shape[: 2]
     if m != 'NA':
-
         # starting point
         px = 0
         py = -(x1-0)*m + y1
@@ -430,43 +127,4 @@ def drawLine(img, x1,y1,x2,y2):
         qx = w
         qy = -(x2-w)*m + y2
         cv2.line(img,(int(px), int(py)), (int(qx),int(qy)), (255,255,255), 12)
-
-    
-x_points = []
-y_points = []
-
-cap = cv2.VideoCapture("straight_simulation.mp4")
-while (cap.isOpened()):
-    _,frame = cap.read()
-    canny_image = canny(frame)
-    cropped_image = region_of_interest(canny_image,1)
-    #cv2.imshow("result", cropped_image)
-    width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float `width`
-    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float `height`
-
-    corners = cv2.goodFeaturesToTrack(cropped_image,3,13,0.05)
-    kernel = np.ones((7,7),np.uint8)
-    corners = cv2.dilate(corners, kernel,iterations = 2)
-    cropped_image[corners>0.025 * corners.max()] = [255,127,127]
-
-    for i in corners:
-        x, y = i.ravel()
-        cv2.circle(frame, (x, y), 3, (0, 255, 0), -1)
-
-    for i in corners:
-        x, y = i.ravel()
-        x_points.append(x)
-        y_points.append(y)
-        cv2.circle(cropped_image, (x, y), 3, 255, -1)
-
-#    print(y_points[2])
-    white = (255,255,255)
-    
-    #drawLine(cropped_image,50,int(height),544,233)
-    cv2.line(cropped_image, (int(x_points[2]),int(y_points[2])), (50,int(height)), white, 15)
-    cv2.imshow("result", cropped_image)
-
-    if cv2.waitKey(5) == ord('q'):
-        break
-cv2.destroyAllWindows()
-'''
+"""    
--- masksForIntersection.py
+++ masksForIntersection.py
@@ -3,132 +3,93 @@ import numpy as np
 from numpy.testing._private.utils import integer_repr
 from matplotlib import pyplot as plt
 
-def canny(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    # reduce noise  -> filter with guassian filter
-    blur = cv2.GaussianBlur(gray,(5,5),0)
-    #canny method to find lanes -> based on colour
-    canny = cv2.Canny(blur, 50, 150)
-    return canny
+class Mask_intesecrtion:
+    """
+    THIS CLASS CREATES THE MASKS NEEDED FOR THE INTERSECTION
+    """
 
-def region_of_interest(image,option):
-    #mask
-    height = image.shape[0]
-    width = image.shape[1]
-    #for options == 1 when the car moves straight
-    if option == 1:
-        polygons=np.array([
-        [(0,height),(int(width/2.5),int(height/3.5)),(int(width/1.6),int(height/3.5)),(width,height)] #(y,x)
-        ])
-    elif option == 2: #options == 2 when the car turn right small
-        polygons=np.array([
-        [(int(width/1.65), int(height/1.68)),(width,int(height/4.1)),(width,height),(int(width/1.65),height)] #(y,x)
-        ])
-    elif option == 3:
-        polygons=np.array([
-        [(1,1),(650,350),(1030,350),(width,height)] #(y,x)
-        ])
+    def canny(image):
+        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        # reduce noise  -> filter with guassian filter
+        blur = cv2.GaussianBlur(gray,(5,5),0)
+        #canny method to find lanes -> based on colour
+        canny = cv2.Canny(blur, 50, 150)
+        return canny
 
-    # apply it to a block mask
-    mask = np.zeros_like(image)
-    cv2.fillPoly(mask, polygons, 255)
-    masked_image = cv2.bitwise_and(image,mask)
-    return masked_image
-    
-## to show the coordinates of an image:
+    def region_of_interest(image,option):
+        #mask
+        height = image.shape[0]
+        width = image.shape[1]
+        #for options == 1 when the car moves straight
+        if option == 1:
+            polygons=np.array([
+            [(0,height),(int(width/2.5),int(height/3.5)),(int(width/1.6),int(height/3.5)),(width,height)] #(y,x)
+            ])
+        elif option == 2: #options == 2 when the car turn right small
+            polygons=np.array([
+            [(int(width/1.65), int(height/3)),(width,int(height/4.1)),(width,height),(int(width/2),height)] #(y,x)
+            ])
+        elif option == 3: #options == 3 when the car turns big left
+            polygons=np.array([
+            [(1,1),(650,350),(1030,350),(width,height)] #(y,x)
+            ])
+        elif option == 4: # to detect the corner in intersection
+            polygons=np.array([
+            [(0,int(height/1.2)),(0,int(height/5)),(int(width/1.4),int(height/5)),(int(width),int(height/1.2))] #(y,x)
+            ])    
 
-
-def click_event(event,x,y,flags,params):
-    #event -> left mouse click
-    if event == cv2.EVENT_LBUTTONDOWN:
-        print(x,' ', y)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(canny_image, str(x) + ',' + str(y), (x,y), font,1,(255,0,0), 2)
-        cv2.imshow('image', canny_image)
-
-
-# HOUGH LINES -> approximate presence of lines in an image
-def houghLines(cropped_canny):
-    return cv2.HoughLinesP(cropped_canny, 2, np.pi/180, 100, np.array([]), minLineLength=40,maxLineGap=5)
-
-#making the points
-def make_points(image, line):
-    slope,intercept = line
-    y1 = int(image.shape[0])
-    y2 = int(y1*3.0/5)
-    x1 = int((y1-intercept)/slope)
-    x2 = int((y2-intercept)/slope)
-    return [[x1,y1,x2,y2]]
-
-#average line -> be more accurate 
-def averaged_slope_intercept(image, lines):
-    left_fit = []
-    right_fit = []
-    if lines is None:
-        return None
-    for line in lines:
-        for x1,y1,x2,y2 in line:
-            fit = np.polyfit((x1,x2), (y1,y2),1)
-            slope = fit[0]
-            intercept = fit[1]
-            if slope < 0:
-                left_fit.append((slope,intercept))
-            if slope >0: 
-                right_fit.append((slope, intercept))
-    left_fit_average = np.average(left_fit, axis = 0)
-    right_fit_average = np.average(right_fit, axis = 0)
-    left_lines = make_points(image, left_fit_average)
-    right_lines = make_points(image, right_fit_average)
-    averaged_lines = [left_lines, right_lines]
-    return averaged_lines
-
-#dispaly the lines
-def display_lines(img, lines):
-    line_img = np.zeros_like(img)
-    if lines is not None:
-        for line in lines:
-            for x1,y1,x2,y2 in line:
-                cv2.line(line_img , (x1,y1), (x2,y2), (0,0,255), 10)
-    return line_img
-
-#to make the line be more visible 
-def addWeighted(frame, line_img):
-    return cv2.addWeighted(frame, 0.8, line_img, 1,1)
+        # apply it to a block mask
+        mask = np.zeros_like(image)
+        cv2.fillPoly(mask, polygons, 255)
+        masked_image = cv2.bitwise_and(image,mask)
+        return masked_image
+        
+    ## to show the coordinates of an image:'''
 
 
-# for reviewing the mask
 
-""" 
-image = cv2.imread("Lanes.png")
-lane_image = np.copy(image)
-canny_image = canny(lane_image)
-cv2.imshow("result", region_of_interest(canny_image,2))
-cv2.waitKey(0)  
-cv2.destroyAllWindows()
+    def click_event(event,x,y,canny_image):
+        #event -> left mouse click
+        if event == cv2.EVENT_LBUTTONDOWN:
+            print(x,' ', y)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(canny_image, str(x) + ',' + str(y), (x,y), font,1,(255,0,0), 2)
+            cv2.imshow('image', canny_image)
+
+    # for reviewing the mask
 """
-# for finding the coordinates of an image
+    image = cv2.imread("photo/rss1.png")
+    lane_image = np.copy(image)
+    canny_image = canny(lane_image)
+    cv2.imshow("result", region_of_interest(canny_image,2))
+    cv2.waitKey(0)  
+    cv2.destroyAllWindows()
+"""
+    # for finding the coordinates of an image
 """ 
-cv2.imshow('image', canny_image)
-cv2.setMouseCallback('image', click_event)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
- """
+    cv2.imshow('image', canny_image)
+    cv2.setMouseCallback('image', click_event)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    """
 
-# for the video
+    # for the video
+"""
+    cap = cv2.VideoCapture("videos/straight_simulation.mp4")
+    while (cap.isOpened()):
+        _,frame = cap.read()
+        canny_image = canny(frame)
+        cropped_image = region_of_interest(canny_image,4)
+        #cv2.imshow("result", cropped_image)
+        width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float `width`
+        height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float `height`  
+        # we iterate through each corner, 
+        # making a circle at each point that we think is a corner.
+        cv2.imshow("result", cropped_image)
 
-cap = cv2.VideoCapture("straight_simulation.mp4")
-while (cap.isOpened()):
-    _,frame = cap.read()
-    canny_image = canny(frame)
-    cropped_image = region_of_interest(canny_image,1)
-    #cv2.imshow("result", cropped_image)
-    width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float `width`
-    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float `height`  
-    # we iterate through each corner, 
-    # making a circle at each point that we think is a corner.
-    cv2.imshow("result", cropped_image)
-
-    if cv2.waitKey(5) == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+        if cv2.waitKey(5) == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    
+    """
