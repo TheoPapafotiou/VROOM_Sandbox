246e46a95e406cf47202eb71741b5a7884a72e76
nassosbountioukos <nassosbountioukos@gmail.com>
2022-03-04 18:11:46 +0200

    random tries

---
+++ depth_camera_test.py
@@ -0,0 +1,77 @@
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+import time
+from sign_detection import SignDetection
+
+sd = SignDetection() # ~ 6 secs delay due to initializations
+
+# Configure depth and color streams
+pipeline = rs.pipeline()
+config = rs.config()
+
+# Get device product line for setting a supporting resolution
+pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_profile = config.resolve(pipeline_wrapper)
+device = pipeline_profile.get_device()
+device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+found_rgb = False
+for s in device.sensors:
+    if s.get_info(rs.camera_info.name) == 'RGB Camera':
+        found_rgb = True
+        break
+if not found_rgb:
+    print("The demo requires Depth camera with Color sensor")
+    exit(0)
+
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+if device_product_line == 'L500':
+    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+else:
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+# Start streaming
+pipeline.start(config)
+count = 0
+try:
+    while count <= 600:
+        count += 1
+        # Wait for a coherent pair of frames: depth and color
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+        if not depth_frame or not color_frame:
+            continue
+
+        # Convert images to numpy arrays
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.1), cv2.COLORMAP_JET)
+
+        depth_colormap_dim = depth_colormap.shape
+        color_colormap_dim = color_image.shape
+
+        # If depth and color resolutions are different, resize color image to match depth image for display
+        if depth_colormap_dim != color_colormap_dim:
+            resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
+            images = np.hstack((resized_color_image, depth_colormap))
+        else:
+            images = np.hstack((color_image, depth_colormap))
+
+        start = time.time()
+        result = sd.detectSign(color_image, 480, 640)
+        print(time.time() - start)
+        print(result)
+        cv2.imwrite('RealSense_Detect.jpg', color_image)
+        
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        # break
+
+finally:
+
+    # Stop streaming
+    pipeline.stop()
--- try2.py
+++ try2.py
@@ -1,6 +1,42 @@
 # from PIL import Image
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+from sys import platform
+if platform == "darwin":
+    mpl.use('macosx')
+
+
+filename1 = "depth/RealSense_4.npy"
+filename2 = "depth/RealSense_11.npy"
+filename3 = "depth/RealSense_13.npy"
+filename4 = "depth/RealSense_15.npy"
+filename5 = "depth/RealSense_16.npy"
+
+filename_empty = "depth/RealSense_try2_2.npy"
+
+
+def point_finding(sample_img, plot_title="click to copy the point"):
+    import pyperclip
+    fig, ax = plt.subplots()
+    plt.title(plot_title)
+    ax.imshow(sample_img)
+
+    def on_click(event):
+        # print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
+        #       ('double' if event.dblclick else 'single', event.button,
+        #        e  vent.x, event.y, event.xdata, event.ydata))
+        print(f"[{int(round(event.xdata, 0))},{int(round(event.ydata, 0))}], {sample_img[int(round(event.ydata, 0))][int(round(event.xdata, 0))]}")
+        pyperclip.copy(f"[{int(round(event.xdata, 0))},{int(round(event.ydata, 0))}]")
+
+    fig.canvas.mpl_connect('button_press_event', on_click)
+    plt.show()
+
+
+def get_image(filename=filename1):
+    return np.load(filename)
 
 
 class Vehicle_detection_depth_cam():
@@ -15,27 +51,26 @@ class Vehicle_detection_depth_cam():
     def __init__(self):
         pass
 
-
     def saliency(self):
 
-        image = self.get_image(self.filename3)
-        empty = self.get_image(self.filename_empty)
-        image = empty
+        image = get_image(self.filename1)
+        empty = get_image(self.filename_empty)
+        # image = empty
 
         # initialize OpenCV's objectness saliency detector and set the path
         # to the input model files
         saliency = cv2.saliency.ObjectnessBING_create()
         saliency.setTrainingPath("model")
-        
+
         # compute the bounding box predictions used to indicate saliency
         (success, saliencyMap) = saliency.computeSaliency(image)
         numDetections = saliencyMap.shape[0]
 
         # loop over the detections
-        for i in range(0, min(numDetections,10)):
+        for i in range(0, min(numDetections, 10)):
             # extract the bounding box coordinates
             (startX, startY, endX, endY) = saliencyMap[i].flatten()
-        
+
             # randomly generate a color for the object and draw it on the image
             output = image.copy()
             color = np.random.randint(0, 255, size=(3,))
@@ -45,12 +80,11 @@ class Vehicle_detection_depth_cam():
             cv2.imshow("Image", output)
             cv2.waitKey(0)
 
-
     def detect(self):
         # frame = cv2.imread('depth/RealSense2_178.jpg', CV_LOAD_IMAGE_ANYCOLOR | CV_LOAD_IMAGE_ANYDEPTH)
         # depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(frame, alpha=0.1), cv2.COLORMAP_JET)
-        image = self.get_image(self.filename3)
-        empty = self.get_image(self.filename_empty)
+        image = get_image(self.filename1)
+        empty = get_image(self.filename_empty)
 
         image[image > 1500] = 3000
         empty[empty > 1500] = 3000
@@ -58,14 +92,31 @@ class Vehicle_detection_depth_cam():
         image1 = empty - image
         image1[image1 > 3000] = 0
 
+        # image = image1
 
 
-        image = image1
-
-
+        ## threshholding
         a = (255 / image.max())
-        print("a= ",a )
+        print("a= ", a)
         image = cv2.convertScaleAbs(image, alpha=a)
+        # point_finding(image)
+        img_thrs = cv2.threshold(image, 110, 255, cv2.THRESH_BINARY)
+        cv2.imshow("thresh", img_thrs[1])
+        cv2.waitKey()
+        img_thrs2 = cv2.threshold(image, 126, 255, cv2.THRESH_BINARY_INV)
+        cv2.imshow("thresh", img_thrs2[1])
+        cv2.waitKey()
+        # img = img_thrs[1]*img_thrs2[1]
+        img = cv2.bitwise_and(img_thrs[1],img_thrs2[1])
+        img = cv2.bitwise_not(img)
+        cv2.imshow("thresh", img)
+        cv2.waitKey()
+
+        image = img
+
+        # a = (255 / image.max())
+        # print("a= ", a)
+        # image = cv2.convertScaleAbs(image, alpha=a)
 
         image2 = cv2.applyColorMap(image, cv2.COLORMAP_HSV)
 
@@ -83,31 +134,34 @@ class Vehicle_detection_depth_cam():
         cv2.imshow("blured", image2)
         cv2.waitKey()
 
+
+
+
         # Setup SimpleBlobDetector parameters.
         params = cv2.SimpleBlobDetector_Params()
 
         # Change thresholds
-        params.minThreshold = 10
-        params.maxThreshold = 200
+        params.minThreshold = 0
+        params.maxThreshold = 255
 
         # Filter by Area.
-        params.filterByArea = True
+        params.filterByArea = False
         params.minArea = 1000
 
         # Filter by Circularity
-        params.filterByCircularity = True
-        params.minCircularity = 0.1
+        params.filterByCircularity = False
+        params.minCircularity = 0.2
 
         # Filter by Convexity
-        params.filterByConvexity = True
+        params.filterByConvexity = False
         params.minConvexity = 0.87
 
         # Filter by Inertia
-        params.filterByInertia = True
-        params.minInertiaRatio = 0.01
+        params.filterByInertia = False
+        params.minInertiaRatio = 0.1
 
         # by color
-        # params.filterByColor = True
+        params.filterByColor = True
 
         # Create a detector with the parameters
         ver = (cv2.__version__).split('.')
@@ -124,10 +178,6 @@ class Vehicle_detection_depth_cam():
         cv2.waitKey(0)
 
 
-    def get_image(self, filename=filename1):
-            return np.load(filename)
-
-
 def canny(image):
     # Returns the processed image
     # gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
@@ -142,6 +192,7 @@ def canny(image):
     return image
 
 
+# point_finding(get_image(filename1))
 v = Vehicle_detection_depth_cam()
-v.saliency()
-# v.detect()
+# v.saliency()
+v.detect()
